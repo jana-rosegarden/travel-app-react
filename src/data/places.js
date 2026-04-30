1dd551dea35d221e8e7565945e0bfb2500c4257e
Jana Engi <128411057+jana-rosegarden@users.noreply.github.com>
2026-04-30 14:19:14 +0100

    Data.js erstellt, Resturants Fotos hinzugefügt

---
+++ src/data/places.js
@@ -0,0 +1,55 @@
+export const restaurants = [
+    {
+        name: "Konoba 'Soprano'",
+        adresse: "Ul. Monte Paradiso 40, 52100, Pula",
+        id: "1Soprano",
+        entfernung: "170m",
+        image: ""
+    },
+    {
+        name: "Pizzeria 'Asterix'",
+        adresse: "Piranesijev prilaz 11, 52100, Pula",
+        id: "2Asterix",
+        entfernung: "1,0km",
+        image: ""
+    },
+    {
+        name: "Pizzeria 'Diego'",
+        adresse: "Ul. de Franceschijeva 23, 52100, Pula",
+        id: "3Diego",
+        entfernung: "1,1km",
+        image: ""
+    },
+    {
+        name: "Fish Food More",
+        adresse: "Rizzijeva ul. 47, 52100, Pula",
+        id: "4Fishfood",
+        entfernung: "1,0km",
+        image: ""
+    },
+    {
+        name: "McDonald's Pula",
+        isFastfood: true,
+        adresse: "Ulica Rimske Centurijacije 101, 52100, Pula",
+        id: "5McdonaldsPula",
+        entfernung: "3,8km",
+        image: ""
+    },
+    {
+        name: "KebaBoss",
+        isFastfood: true,
+        adresse: "Tomasinijeva ul. 34, 52100, Pula",
+        id: "5McdonaldsPula",
+        entfernung: "800m",
+        image: ""
+    },
+     {
+        name: "Sandwich Club",
+        isFastfood: true,
+        adresse: "Sisplac ul. 4, 52100, Pula",
+        id: "5McdonaldsPula",
+        entfernung: "1,2km",
+        image: ""
+    }
+]
+
