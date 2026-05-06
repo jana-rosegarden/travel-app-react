6629d4c294eb5105e7dad2280b0f8f2dbb0921c2
Jana Engi <128411057+jana-rosegarden@users.noreply.github.com>
2026-05-06 15:27:40 +0100

    Strands Datei erstellt, ausgefüllt, Fotos hinzugefügt

---
+++ src/data/strands.js
@@ -0,0 +1,104 @@
+export const strands = [
+    {
+        name: {
+            de: "kkk",
+            uk: " "
+        },
+        id: "KKK",
+        entfernung: 22,
+        isKostenfrei: true,
+        parken: {
+            de: "kkk",
+            uk: "..."
+        },
+        info: {
+            de: "kkkk",
+            uk: "kkkk"
+        }
+    },
+    {
+        name: {
+            de: "Valsaline (Mornar)",
+            uk: "Валсаліне "
+        },
+        id: "valsaline",
+        entfernung: 0.8,
+        isKostenfrei: true,
+        parken: {
+            de: "In der näheren Umgebung sind kostenlose Parkplätze vorhanden",
+            uk: "У найближчих околицях є безкоштовні паркувальні місця"
+        },
+        info: {
+            de: "Beliebter Kiesel- und Sandstrand am Ende der Lungomare-Promenade, ideal für Familien und Sonnenanbeter. Er zeichnet sich durch flach abfallendes Wasser und eine angrenzende Waldfläche. Es gibt dort Gastronomie, Duschen und Wassersportmöglichkeiten",
+            uk: "Популярний гальково-піщаний пляж у кінці набережної Лунгомаре, ідеальний для сімей та любителів сонячних ванн. Він вирізняється пологим входом у воду та прилеглим лісовим масивом. Тут є заклади харчування, душові та можливості для занять водними "
+        }
+    },
+    {
+        name: {
+            de: "Gortanova Cove",
+            uk: "Гортанова Бухта"
+        },
+        id: "gortan-cove",
+        entfernung: 3.2,
+        isKostenfrei: true,
+        parken: {
+            de: "Das Parken ist direkt an der Straße Lungomare möglich. Dies ist jedoch gebührenpflichtig. Da der Strand sehr beliebt ist, wird empfohlen, frühzeitig zu kommen. ",
+            uk: "Паркуватися можна безпосередньо на вулиці Лунгомаре. Однак це платна послуга. Оскільки пляж дуже популярний, рекомендуємо приїжджати заздалегідь. "
+        },
+        info: {
+            de: "Ein malerischer, familienfreundlicher Kieselstrand an der Promenade Lungomare, umgeben von schattigen Pinienwäldern. Er bietet einen flachen Einstieg ins kristallklare Wasser, ist ideal für Kinder und Schnorchler. Der Strand bietet eine entspannte Atmosphäre, eine kleine Strandbar und ist ein beliebter Ort für Einheimische und Touristen.",
+            uk: "Мальовничий гальковий пляж, ідеальний для сімейного відпочинку, розташований на набережній Лунгомаре в оточенні тінистих соснових лісів. Він має пологий спуск у кришталево чисту воду, що робить його ідеальним для дітей та любителів снорклінгу. Пляж вирізняється спокійною атмосферою, має невеликий пляжний бар і є улюбленим місцем як для місцевих жителів, так і для туристів."
+        }
+    },
+    {
+        name: {
+            de: "Valkane Beach",
+            uk: "Пляж Валкане"
+        },
+        id: "valkane-beach",
+        entfernung: 2.2,
+        isKostenfrei: true,
+        parken: {
+            de: "Kostenlose Parkplätze stehen direkt neben dem Pinienwald zur Verfügung und bieten schattige Stellplätze nur wenige Gehminuten vom Meer entfernt. An Sommerwochenenden kann der Parkplatz schnell voll sein, daher wird eine frühzeitige Ankunft empfohlen.",
+            uk: "Безкоштовні паркувальні місця розташовані безпосередньо біля соснового лісу; це затінені стоянки, що знаходяться всього за декілька хвилин ходьби від моря. У літні вихідні парковка може швидко заповнитися, тому рекомендуємо приїжджати заздалегідь."
+        },
+        info: {
+            de: "Der Valkane Strand in Pula ist ein beliebter, gut ausgestatteter Stadtstrand (mit Blauer Flagge) in einer Bucht, der Kies-, Beton- und Felsabschnitte bietet. Ausstattung: Duschen, Umkleidekabinen, Kinderspielplatz, Beachvolleyball- und Tennisplätze in der Nähe. Gastronomie: Zwei Beach-Bars/Restaurants vor Ort.",
+            uk: "Пляж «Валкане» в Пулі — це популярний, добре облаштований міський пляж (відзначений «Блакитним прапором»), розташований у бухті, що має ділянки з галькою, бетоном та скелями. Обладнання: душові, роздягальні, дитячий майданчик, майданчики для пляжного волейболу та тенісу поблизу. Харчування: два пляжні бари/ресторани на території."
+        }
+    },
+    {
+        name: {
+            de: "Plaža Ambrela",
+            uk: "Пляж Амбрела"
+        },
+        id: "ambrela",
+        entfernung: 1.7,
+        isKostenfrei: true,
+        parken: {
+            de: "In der Nähe der Küste gibt es einen öffentlichen Parkplatz, der jedoch in den Sommermonaten schnell belegt ist.",
+            uk: "Неподалік від узбережжя є громадська автостоянка, яка, однак, у літні місяці швидко заповнюється."
+        },
+        info: {
+            de: "Der Ambrela Strand (Plaža Ambrela) auf der Verudela-Halbinsel in Pula ist ein beliebter, mit der Blauen Flagge zertifizierter Kiesstrand. Er ist besonders für Familien geeignet, da das Wasser flach abfällt. Er bietet Pinienbäume für Schatten, Rettungsschwimmer, Duschen, Liegenverleih und Bars. ",
+            uk: "Пляж Амбрела (Plaža Ambrela) на півострові Верудела в Пулі — це популярний гальковий пляж, сертифікований «Блакитним прапором». Він особливо підходить для сімейного відпочинку, оскільки дно поглиблюється плавно. Тут є сосни, що дають прохолодну тінь, рятувальники, душові, прокат шезлонгів та бари. "
+        }
+    },
+    {
+        name: {
+            de: "Histria Beach",
+            uk: "Пляж Істрія"
+        },
+        id: "histria-beach",
+        entfernung: 2.2,
+        isKostenfrei: true,
+        parken: {
+            de: "Es gibt zwei bequeme Alternativen: Option 1: Kostenlose Parkplätze in der Nähe des E&D Caffe – nur wenige Gehminuten vom Strand entfernt. Option 2: Bitten Sie den Wachmann am Eingang des Verudela-Camps um Einlass; innerhalb des Resortgeländes stehen mehrere Parkplätze zur Verfügung.",
+            uk: "Є дві зручні альтернативи: Варіант 1: Безкоштовні паркувальні місця поблизу E&D Caffe — лише за кілька хвилин ходьби від пляжу. Варіант 2: Попросіть охоронця біля входу в курорт Verudela пропустити вас; на території курорту є кілька паркувальних місць."
+        },
+        info: {
+            de: "Der Strand ist eine Mischung aus Kiesabschnitten und Felsplateaus.Kristallklares Wasser: Ausgezeichnet mit der Blauen Flagge für hohe Wasserqualität.Flacher Einstieg: Der Kiesstrand fällt sanft ins Meer ab, ideal für Kinder und Senioren.Natürlicher Schatten: Pinienbäume im Hintergrund bieten Schutz vor der Mittagssonne",
+            uk: "Пляж складається з ділянок з галькою та скелястих плато. Кришталево чиста вода: нагороджений «Блакитним прапором» за високу якість води. Пологий спуск: гальковий пляж плавно спускається до моря, що ідеально підходить для дітей та людей похилого віку. Природна тінь: сосни на задньому плані забезпечують захист від полуденного сонця"
+        }
+    },
+]
