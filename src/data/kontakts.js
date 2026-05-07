b04901edf80495bdbcf308e4f679879cc4654c3a
Jana Engi <128411057+jana-rosegarden@users.noreply.github.com>
2026-05-07 14:51:45 +0100

    Kontakte hinzugefügt

---
+++ src/data/kontakts.js
@@ -0,0 +1,44 @@
+export const telefonNummer = [
+    {
+        name:{
+            de: "Jana",
+            ukr: "Яна"
+        },
+        telefon: 4915901632722
+    },
+    {
+        name:{
+            de: "Andreas",
+            ukr: "Адреас"
+        },
+        telefon: 4917623740505
+    },
+    {
+        name:{
+            de: "Marian",
+            ukr: "Мар'ян"
+        },
+        telefon: 4915566324171
+    },
+    {
+        name:{
+            de: "Vera",
+            ukr: "Віра"
+        },
+        telefon: 495252977674
+    },
+    {
+        name:{
+            de: "Ihor",
+            ukr: "Ігор"
+        },
+        telefon: 491626615039
+    },
+    {
+        name:{
+            de: "Nazar",
+            ukr: "Назар"
+        },
+        telefon: 48729498910
+    }
+]
