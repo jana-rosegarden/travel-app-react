fd4173a0aa91d9d59a01020fda5bdc00e215c4a2
Jana Engi <128411057+jana-rosegarden@users.noreply.github.com>
2026-05-11 13:12:32 +0100

    Shops mit Foto hinzugefügt

---
+++ src/data/shops.js
@@ -0,0 +1,93 @@
+import grgaImage from "../assets/images/shops/grga.webp"
+import komsumImage from "../assets/images/shops/konsum.webp"
+import marymatImage from "../assets/images/shops/marymat.webp"
+import studenacImage from "../assets/images/shops/studenac.webp"
+
+
+export const shops = [
+    {
+        name: "kk",
+        adresse: {
+            de: "kkk",
+            ukr: "kkk"
+        },
+        entfernung: kkk,
+        oeffnungszeiten: {
+            de: "kkkk",
+            ukr: "jjj"
+        },
+        info: {
+            de: "jjj",
+            ukr: "kkk"
+        },
+        image: kkk
+    },
+    {
+        name: "Grga Grocery store",
+        adresse: {
+            de: "Ul. Monte Paradiso 11, 52100, Pula",
+            ukr: "Вул. Монте Парадізо, 11, 52100, Пула"
+        },
+        entfernung: 0.1,
+        oeffnungszeiten: {
+            de: "07:00 - 20:30. Sonntags geschlossen",
+            ukr: "07:00 - 20:30. У неділю закрито"
+        },
+        info: {
+            de: "Der Grga Grocery Store in Pula ist ein lokaler Lebensmittelmarkt, der besonders bei Anwohnern und Touristen in der Nähe der Marina Veruda beliebt ist. Das Sortiment umfasst alles, was man für den täglichen Bedarf oder den Urlaub benötigt. Preise: Oft etwas teurer als bei Discountern (ca. 20–25 % über Lidl-Niveau), dafür oft mit besserer Qualität bei Fleisch und Gemüse.",
+            ukr: "Продуктовий магазин «Grga» у Пулі — це місцевий продовольчий ринок, який користується особливою популярністю серед місцевих жителів та туристів, що перебувають поблизу пристані «Марина Веруда». Асортимент включає все необхідне для щоденних потреб або відпочинку. Ціни: зазвичай дещо вищі, ніж у дискаунтерах (приблизно на 20–25 % вище рівня «Lidl»), зате м’ясо та овочі часто кращої якості."
+        },
+        image: kkk
+    },
+    {
+        name: "Studenac Market",
+        adresse: {
+            de: "Gervaisova ulica 11, 52100, Pula",
+            ukr: "Вулиця Герваїсова, 11, 52100, Пула"
+        },
+        entfernung: 0.9,
+        oeffnungszeiten: {
+            de: "07:00 - 21:00. Sonntags geschlossen",
+            ukr: "07:00 - 21:00. У неділю закрито"
+        },
+        info: {
+            de: "Der Studenac Market ist ein typischer, kleiner lokaler Supermarkt, der sich ideal für den täglichen Bedarf eignet. Man kann dort frisches Brot und Backwaren, Obst und Gemüse, Getränke, Konserven, Snacks sowie grundlegende Hygieneartikel kaufen.",
+            ukr: "«Studenac Market» — це типовий невеликий місцевий супермаркет, який ідеально підходить для щоденних покупок. Там можна придбати свіжий хліб та випічку, фрукти й овочі, напої, консерви, снеки, а також основні засоби гігієни."
+        },
+        image: kkk
+    },
+    {
+        name: "Marymat",
+        adresse: {
+            de: "Voltićeva ul. 5, 52100, Pula",
+            ukr: "Вулиця Вольтичева, 5, 52100, Пула"
+        },
+        entfernung: 0.7,
+        oeffnungszeiten: {
+            de: "06:00 - 20:20, samstags und sontags: 06:00 - 13:00",
+            ukr: "06:00 — 20:20, у суботу та неділю: 06:00 — 13:00"
+        },
+        info: {
+            de: "Der Laden ist klein, aber gut sortiert und bietet eine Mischung aus Lebensmitteln und Haushaltswaren. Oft findet man dort auch regionale Produkte wie istrisches Olivenöl oder lokale Weine, die meist günstiger sind als in den Souvenirshops in der Innenstadt.",
+            ukr: "Магазин невеликий, але добре укомплектований і пропонує широкий асортимент продуктів харчування та товарів для дому. Часто там можна знайти й регіональні товари, такі як істрійська оливкова олія або місцеві вина, які зазвичай коштують дешевше, ніж у сувенірних крамницях у центрі міста."
+        },
+        image: kkk
+    },
+    {
+        name: "Konzum",
+        adresse: {
+            de: "Tomasinijeva ul. 26, 52100, Pula",
+            ukr: "Вул. Томасіні, 26, 52100, Пула"
+        },
+        entfernung: 1.2,
+        oeffnungszeiten: {
+            de: "07:00 - 20:00, sontags - geschlossen",
+            ukr: "07:00 – 20:00, у неділю — зачинено"
+        },
+        info: {
+            de: "Konzum ist eine der größten Supermarktketten in Kroatien und bietet in Pula ein breites Sortiment an Lebensmitteln und Haushaltswaren. Besonders für Urlauber ist es eine beliebte Anlaufstelle für lokale Spezialitäten und den täglichen Bedarf. Konzum legt großen Wert auf regionale Produkte, die sich auch hervorragend als Mitbringsel eignen",
+            ukr: "Konzum — одна з найбільших мереж супермаркетів у Хорватії, яка пропонує в Пулі широкий асортимент продуктів харчування та товарів для дому. Особливо серед туристів це популярне місце, де можна придбати місцеві делікатеси та товари щоденного вжитку. Konzum приділяє велику увагу регіональним продуктам, які чудово підходять як сувеніри"
+        },
+        image: kkk
+    },
+]
