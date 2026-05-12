5dc5ce8d722773434f068696b77c1f55e60e89ad
Jana Engi <128411057+jana-rosegarden@users.noreply.github.com>
2026-05-12 11:05:40 +0100

    Ärzte hinzugefügt

---
+++ src/data/aerzte.js
@@ -0,0 +1,48 @@
+export const aerzte = [
+    {
+        name:{
+            de: "Opca medicina",
+            ukr: "Загальна медицина"
+        },
+        adresse: {
+            de: "Veruda ul., 52100, Pula",
+            ukr: "Вул. Веруда, 52100, Пула"
+        },
+        entfernung: 1.2,
+        oefnnungszeiten: {
+            de: "Es handelt sich um eine allgemeinmedizinische Praxis, die typischerweise vormittags und nachmittags geöffnet ist.",
+            ukr: "Це поліклініка загального профілю, яка зазвичай працює вранці та вдень."
+        }
+    },
+    {
+        name:{
+            de: "Butkovic Dr. Morena",
+            ukr: "Butkovic Dr. Morena"
+        },
+        adresse: {
+            de: "Palladiova ul. 22, 52100, Pula",
+            ukr: "Вул. Палладіова, 22, 52100, Пула"
+        },
+        entfernung: 1.0,
+        oefnnungszeiten: {
+            de: "Ungerade Kalendertage: Vormittags von 07:00 bis 14:30 Uhr. Gerade Kalendertage: Nachmittags von 13:00 bis 20:30 Uhr.",
+            ukr: "Непарні календарні дні: вранці з 07:00 до 14:30. Парні календарні дні: після обіду з 13:00 до 20:30."
+        }
+    },
+    {
+        name:{
+            de: "Dr. Mirjana Barac Santini",
+            ukr: "Dr. Mirjana Barac Santini"
+        },
+        adresse: {
+            de: "Tommaseova ul. 11, 52100, Pula",
+            ukr: "Вул. Томмазеова, 11, 52100, Пула"
+        },
+        entfernung: 1.3,
+        oefnnungszeiten: {
+            de: "Gerade Daten: 08:00 – 13:00 Uhr. Ungerade Daten: 14:00 – 19:00 Uhr",
+            ukr: "Парні дні: 08:00 – 13:00. Непарні дні: 14:00 – 19:00"
+        }
+    }
+    
+]
