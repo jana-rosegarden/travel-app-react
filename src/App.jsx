0f02028e876e152509cfc3793c7b50da0eae4ab3
Jana Engi <128411057+jana-rosegarden@users.noreply.github.com>
2026-05-13 14:02:35 +0100

    React Router installiert, Header Komponente erstellt

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import { useState } from 'react'
+import { BrowserRouter, Routes,  Route, Link, NavLink} from 'react-router-dom'
 
 import './App.css'
+
+import Header from './components/Header.jsx'
 
 function App() {
   
 
   return (
-      <>
-      </>
+      <BrowserRouter>
+          <Header />
+      </BrowserRouter>
   )
 }
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+export default function Header(){
+    return(
+        <header className="header-main">
+            <h1 className="header-main__h1"> PULA </h1>
+        </header>
+    )
+}
