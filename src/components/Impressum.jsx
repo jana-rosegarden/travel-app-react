611e56eb0d5af2eeeaeb857baf792a3daf670a0f
Jana Engi <128411057+jana-rosegarden@users.noreply.github.com>
2026-06-20 17:06:39 +0100

    Datenschutz & Impressum - dynamische Sprachenweckslung entwicklet

--- src/components/Impressum.jsx
+++ src/components/Impressum.jsx
@@ -2,44 +2,36 @@ import { useContext } from "react";
 import { LanguageContext } from "../App";
 
 import { footer } from "../data/translations.js";
-
+import { translations } from "../data/translations.js";
 
 export default function Impressum({toggleDaten, toggleImpressum, showDaten, showImpressum}){
     const {lang, setLang} = useContext(LanguageContext);
     //console.log(footer.impressum[lang])
 
-
     return(
             <>
                 <div > 
-                    <h3 onClick={toggleDaten}>Datenschutzerklärung</h3>
+                    <h3 onClick={toggleDaten}> {translations[lang].datenschutz} </h3>
                     {showDaten && 
                     <div>
                         {footer.datenschutz[lang].map(item=>{
                             return(
-                                <p key={item.id}>{item.text}</p>
+                                <div key={item.id}> 
+                                    <p>{item.text}</p>
+                                    <div>{item.textData && item.textData.map(data=>{
+                                        return(
+                                            <p key={data.id}> {data.data} </p>
+                                        )
+                                    })}</div>
+                                </div>
                             )
                         })}
-                        {/*<p>
-                            Diese Webseite erhebt keine personenbezogenen Daten und verwendet keine Cookies oder Tracking-Tools.
-                            Es werden keine externen Dienste wie Google Analytics, YouTube, Facebook oder ähnliche Anbieter eingebunden.
-                        </p>
-                        <p>
-                            Wenn Sie auf einen Link zu externen Webseiten (z. B. Instagram oder GitHub) klicken, verlassen Sie meine Seite.
-                            Für die Inhalte und den Datenschutz dieser externen Seiten bin ich nicht verantwortlich.
-                        </p>
-                        <p>
-                            Verantwortlich für den Inhalt dieser Seite:<br />
-                            Jana Engenhorst<br />
-                            Josef-Priller-Str. 20<br />
-                            86159 Augsburg<br />
-                            E-Mail: jana@engenhor.st <br />
-                        </p> */}
+                        
                     </div>
                     }
 
                     <div > 
-                    <h3 onClick={toggleImpressum}> Impressum </h3>
+                    <h3 onClick={toggleImpressum}> {translations[lang].impressum} </h3>
                     {showImpressum && 
                         <div > 
                             {footer.impressum[lang].map(item =>{
@@ -47,9 +39,7 @@ export default function Impressum({toggleDaten, toggleImpressum, showDaten, show
                                     <p key={item.id}>{item.text}</p>
                                 )
                             })}
-                            {/*<p>Jana Engenhorst</p>
-                            <p>Josef-Priller-Str. 20, 86159 Augsburg </p>
-                            <p>jana@engenhor.st</p> */}
+
                         </div>
                     }
                     </div>
--- src/data/translations.js
+++ src/data/translations.js
@@ -11,7 +11,9 @@ export const translations = {
         info: "Info",
         alterVon: "Für welches Alter geeignet",
         parken: "Parkensmöglichkeiten",
-        eintritt: "Eintritt"
+        eintritt: "Eintritt",
+        datenschutz: "Datenschutzerklärung",
+        impressum: "impressum"
     },
     uk:{
         name: "Назва",
@@ -22,7 +24,9 @@ export const translations = {
         info: "Інфо",
         alterVon: "Для якого віку",
         parken: "Парковка",
-        eintritt: "Вхід"
+        eintritt: "Вхід",
+        datenschutz: "Політика конфіденційності",
+        impressum: "Вихідні дані"
     }
 };
 
@@ -55,18 +59,23 @@ export const footer = {
     uk: [{text: "Яна Енгенхорст", id: 234}, {text: "Josef-Priller-Str. 20, 86159 Аугсбург", id: 345}, {text: "jana@engenhor.st", id: 567}]
    },
    datenschutz:{
-    de: [{text: "Diese Webseite erhebt keine personenbezogenen Daten und verwendet keine Cookies oder Tracking-Tools. Es werden keine externen Dienste wie Google Analytics, YouTube, Facebook oder ähnliche Anbieter eingebunden.", id: 111}, {text: "Wenn Sie auf einen Link zu externen Webseiten (z. B. Instagram oder GitHub) klicken, verlassen Sie meine Seite. Für die Inhalte und den Datenschutz dieser externen Seiten bin ich nicht verantwortlich.", id: 222},
-        {text: [{data: "Verantwortlich für den Inhalt dieser Seite:", id: 11}, 
-                {data:"Jana Engenhorst", id: 22}, {data: "Josef-Priller-Str. 20", id: 33,},
-                {data: "86159 Augsburg", id: 44},
-                {data: "E-Mail: jana@engenhor.st", id: 55}]}
+    de: [{text: "Diese Webseite erhebt keine personenbezogenen Daten und verwendet keine Cookies oder Tracking-Tools. Es werden keine externen Dienste wie Google Analytics, YouTube, Facebook oder ähnliche Anbieter eingebunden.", id: 111}, 
+        {text: "Wenn Sie auf einen Link zu externen Webseiten (z. B. Instagram oder GitHub) klicken, verlassen Sie meine Seite. Für die Inhalte und den Datenschutz dieser externen Seiten bin ich nicht verantwortlich.", id: 222},
+        {textData: [{data: "Verantwortlich für den Inhalt dieser Seite:", id: 11}, 
+                    {data:"Jana Engenhorst", id: 22}, 
+                    {data: "Josef-Priller-Str. 20", id: 33,},
+                    {data: "86159 Augsburg", id: 44},
+                    {data: "E-Mail: jana@engenhor.st", id: 55}],
+                id: 201}
     ],
     uk: [{text: "Цей додаток не збирає персональних даних і не використовує файли cookie чи засоби відстеження. У ньому не використовуються зовнішні сервіси, такі як Google Analytics, YouTube, Facebook чи подібні сервіси.", id: 333}, {text: "Якщо ви натиснете на посилання на зовнішні веб-сайти (наприклад, Instagram або GitHub), ви покинете мій сайт. Я не несу відповідальності за вміст та політику конфіденційності цих зовнішніх сайтів.", id: 444},
-        {text: [{data: "Verantwortlich für den Inhalt dieser Seite:", id: 11}, 
-                {data:"Jana Engenhorst", id: 22}, {data: "Josef-Priller-Str. 20", id: 33,},
-                {data: "86159 Augsburg", id: 44},
-                {data: "E-Mail: jana@engenhor.st", id: 55}]}
-        
-     ]
+        {textData: [{data: "Verantwortlich für den Inhalt dieser Seite:", id: 66}, 
+                    {data:"Яна Енгенхорст", id: 77}, 
+                    {data: "Josef-Priller-Str. 20", id: 88,},
+                    {data: "86159 Augsburg", id: 99},
+                    {data: "Електронна пошта: jana@engenhor.st", id: 101}],
+                 id: 401
+                }
+     ],
    }
 }
